79b397b2388823eeb1653fd803609e4443f1898c
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2022-09-03 12:32:06 -0500

    Updating repository resource

--- src/controllers/repository/repository.controller.ts
+++ src/controllers/repository/repository.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RepositoryService } from './repository.service';
 import { CreateRepositoryDto } from './dto/create-repository.dto';
 import { UpdateRepositoryDto } from './dto/update-repository.dto';
@@ -7,28 +15,8 @@ import { UpdateRepositoryDto } from './dto/update-repository.dto';
 export class RepositoryController {
   constructor(private readonly repositoryService: RepositoryService) {}
 
-  @Post()
-  create(@Body() createRepositoryDto: CreateRepositoryDto) {
-    return this.repositoryService.create(createRepositoryDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.repositoryService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.repositoryService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRepositoryDto: UpdateRepositoryDto) {
-    return this.repositoryService.update(+id, updateRepositoryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.repositoryService.remove(+id);
   }
 }
--- src/controllers/repository/repository.service.ts
+++ src/controllers/repository/repository.service.ts
@@ -4,23 +4,7 @@ import { UpdateRepositoryDto } from './dto/update-repository.dto';
 
 @Injectable()
 export class RepositoryService {
-  create(createRepositoryDto: CreateRepositoryDto) {
-    return 'This action adds a new repository';
-  }
-
-  findAll() {
-    return `This action returns all repository`;
-  }
-
   findOne(id: number) {
     return `This action returns a #${id} repository`;
-  }
-
-  update(id: number, updateRepositoryDto: UpdateRepositoryDto) {
-    return `This action updates a #${id} repository`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} repository`;
   }
 }
