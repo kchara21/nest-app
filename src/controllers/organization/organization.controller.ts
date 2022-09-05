775a2d806d04427dfbfb2e7e7327369f7395fbae
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2022-09-05 00:25:40 -0500

    Adding HttpExceptionFilter to organization.controller.ts

--- src/controllers/organization/organization.controller.ts
+++ src/controllers/organization/organization.controller.ts
@@ -7,33 +7,41 @@ import {
   Param,
   Delete,
   Req,
+  UseFilters,
 } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
 import { Organization } from './entities/organization.entity';
 import { Request } from 'express';
+import { ApiResponse } from '../../shared/dto/api-response.dto';
+import { HttpExceptionFilter } from '../../core/filters/http-exception.filter';
+
 @Controller('organizations')
 export class OrganizationController {
   constructor(private readonly organizationService: OrganizationService) {}
 
   @Post()
-  create(@Body() createOrganizationDto: CreateOrganizationDto) {
-    const newOrganization: CreateOrganizationDto = createOrganizationDto;
-    return this.organizationService.create(newOrganization);
+  @UseFilters(new HttpExceptionFilter())
+  create(@Body() organization: CreateOrganizationDto) {
+    this.organizationService.create(organization);
+    return new ApiResponse('Usuario creado exitosamente');
   }
 
   @Get()
+  @UseFilters(new HttpExceptionFilter())
   findAll(@Req() request: Request): Promise<Organization[]> {
     return this.organizationService.findAll(request.query);
   }
 
   @Get(':id')
+  @UseFilters(new HttpExceptionFilter())
   findOne(@Param('id') id: string): Promise<Organization> {
     return this.organizationService.findOne(id);
   }
 
   @Patch(':id')
+  @UseFilters(new HttpExceptionFilter())
   update(
     @Param('id') id: string,
     @Body() updateOrganizationDto: UpdateOrganizationDto,
