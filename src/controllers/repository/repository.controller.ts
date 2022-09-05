c68c291ed8e21c648b0f644469264bae1f3aba2d
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2022-09-05 00:23:38 -0500

    Adding HttpExceptionFilter to repository.controller.ts

--- src/controllers/repository/repository.controller.ts
+++ src/controllers/repository/repository.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseFilters } from '@nestjs/common';
 import { RepositoryService } from './repository.service';
+import { HttpExceptionFilter } from '../../core/filters/http-exception.filter';
+import { ApiResponse } from '../../shared/dto/api-response.dto';
 
 @Controller('repositories')
 export class RepositoryController {
   constructor(private readonly repositoryService: RepositoryService) {}
 
   @Get('tribe/:id')
+  @UseFilters(new HttpExceptionFilter())
   findAll(@Param('id') id: string) {
     return this.repositoryService.findRepositoriesByTribe(id);
   }
 
   @Get('tribe/:id/report')
   exportReport(@Param('id') id: string) {
-    return this.repositoryService.exportReportByTribe(id);
+    this.repositoryService.exportReportByTribe(id);
+    return new ApiResponse('Exportacion exitosa');
   }
 }
