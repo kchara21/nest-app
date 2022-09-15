88395db0fb3e7a38ca4fdd858acd3a412e936cb5
kchara21 <kalebdavidchara@hotmail.com>
2022-09-15 11:27:25 -0500

    Optimizing each case using jest.spyOn

--- src/controllers/repository/repository.service.spec.ts
+++ src/controllers/repository/repository.service.spec.ts
@@ -3,12 +3,10 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { RepositoryService } from './repository.service';
 import { Metric } from '../metric/entities/entity.entity';
 import { Repository as Repo } from './entities/repository.entity';
+import { createMock } from '@golevelup/ts-jest';
 
-//Obtener métricas de repositorios por tribu
-describe('find metrics by repository id', () => {
-  let service: RepositoryService;
-
-  const mockRepositoriesByTribe = {
+describe('Repository Service', () => {
+  const mockMetricByTribe = {
     id_metric: 25,
     coverage: '76.00',
     bugs: 5,
@@ -34,15 +32,17 @@ describe('find metrics by repository id', () => {
     },
   };
 
-  const mockRepo = {
+  const mockMetricRepository = {
     createQueryBuilder: jest.fn(() => ({
       innerJoinAndSelect: jest.fn().mockReturnThis(),
       where: jest.fn().mockReturnThis(),
       andWhere: jest.fn().mockReturnThis(),
-      getMany: jest.fn().mockReturnValue([mockRepositoriesByTribe]),
+      getMany: jest.fn().mockReturnValue([mockMetricByTribe]),
       filter: jest.fn().mockReturnThis(),
     })),
   };
+
+  let service: RepositoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -50,11 +50,11 @@ describe('find metrics by repository id', () => {
         RepositoryService,
         {
           provide: getRepositoryToken(Metric),
-          useValue: mockRepo,
+          useValue: mockMetricRepository,
         },
         {
           provide: getRepositoryToken(Repo),
-          useValue: mockRepo,
+          useValue: createMock(),
         },
       ],
     }).compile();
@@ -62,128 +62,76 @@ describe('find metrics by repository id', () => {
     service = module.get<RepositoryService>(RepositoryService);
   });
 
+  it('Should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
   it('Obtener métricas de repositorios por tribu', async () => {
+    const idTribe = 23;
     try {
-      const metricsResponse = await service.findRepositoriesByTribe(23 + '');
-      expect(metricsResponse).toEqual([mockRepositoriesByTribe]);
+      const metricsResponse = await service.findRepositoriesByTribe(
+        idTribe + '',
+      );
+      expect(metricsResponse).toEqual([mockMetricByTribe]);
     } catch (e) {
       expect(e.message).toEqual('La Tribu no se encuentra registrada');
     }
   });
-});
 
-//Tribu inexistente
-describe('find metrics by repository id', () => {
-  let service: RepositoryService;
-
-  const mockRepo = {
-    createQueryBuilder: jest.fn(() => ({
+  it('Tribu inexistente', async () => {
+    const idTribe = 23;
+    jest.spyOn(mockMetricRepository, 'createQueryBuilder').mockReturnValueOnce({
       innerJoinAndSelect: jest.fn().mockReturnThis(),
       where: jest.fn().mockReturnThis(),
       andWhere: jest.fn().mockReturnThis(),
       getMany: jest.fn().mockReturnValue([]),
       filter: jest.fn().mockReturnThis(),
-    })),
-  };
+    });
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        RepositoryService,
-        {
-          provide: getRepositoryToken(Metric),
-          useValue: mockRepo,
-        },
-        {
-          provide: getRepositoryToken(Repo),
-          useValue: mockRepo,
-        },
-      ],
-    }).compile();
-
-    service = module.get<RepositoryService>(RepositoryService);
-  });
-
-  it('Tribu inexistente', async () => {
     try {
-      await service.findRepositoriesByTribe(23 + '');
+      await service.findRepositoriesByTribe(idTribe + '');
     } catch (e) {
       expect(e.message).toEqual('La Tribu no se encuentra registrada');
     }
   });
 
-  // it('should throw exception "La Tribu no tiene repositorios que cumplan con la cobertura necesaria"', async () => {
-  //   const repositoryFound = await service.findRepositoriesByTribe(23 + '');
-  //   expect(repositoryFound).toMatchObject(mockRepositoriesByTribe);
-  // });
-
-  // it('should throw exception "La Tribu no se encuentra registrada"', async () => {
-  //   const tribe = 24;
-  // });
-});
-
-//Tribu no tiene repositorios que cumplan con la cobertura
-describe('find metrics by repository id', () => {
-  let service: RepositoryService;
-
-  const mockRepositoriesByTribe = {
-    id_metric: 25,
-    coverage: '73.00',
-    bugs: 5,
-    vulnerabilities: 3,
-    hotspot: 2,
-    code_smells: 1,
-    repository: {
-      id_repository: 29,
-      name: 'repo_nttdata2',
-      state: 'E',
-      create_time: '2022-08-28T18:33:58.501Z',
-      status: 'A',
-      tribe: {
-        id_tribe: 23,
-        name: 'tribe_front_02',
-        status: 1,
-        organization: {
-          id_organization: 27,
-          name: 'kchara',
+  it('Tribu no tiene repositorios que cumplan con la cobertura', async () => {
+    const mockMetricByTribeWithoutCoverage = {
+      id_metric: 25,
+      coverage: '74.00',
+      bugs: 5,
+      vulnerabilities: 3,
+      hotspot: 2,
+      code_smells: 1,
+      repository: {
+        id_repository: 29,
+        name: 'repo_nttdata2',
+        state: 'E',
+        create_time: '2022-08-28T18:33:58.501Z',
+        status: 'A',
+        tribe: {
+          id_tribe: 23,
+          name: 'tribe_front_02',
           status: 1,
+          organization: {
+            id_organization: 27,
+            name: 'kchara',
+            status: 1,
+          },
         },
       },
-    },
-  };
+    };
 
-  const mockRepo = {
-    createQueryBuilder: jest.fn(() => ({
+    jest.spyOn(mockMetricRepository, 'createQueryBuilder').mockReturnValueOnce({
       innerJoinAndSelect: jest.fn().mockReturnThis(),
       where: jest.fn().mockReturnThis(),
       andWhere: jest.fn().mockReturnThis(),
-      getMany: jest.fn().mockReturnValue([mockRepositoriesByTribe]),
+      getMany: jest.fn().mockReturnValue([mockMetricByTribeWithoutCoverage]),
       filter: jest.fn().mockReturnThis(),
-    })),
-  };
+    });
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        RepositoryService,
-        {
-          provide: getRepositoryToken(Metric),
-          useValue: mockRepo,
-        },
-        {
-          provide: getRepositoryToken(Repo),
-          useValue: mockRepo,
-        },
-      ],
-    }).compile();
-
-    service = module.get<RepositoryService>(RepositoryService);
-  });
-
-  it('Tribu no tiene repositorios que cumplan con la cobertura', async () => {
     try {
-      const metricsResponse = await service.findRepositoriesByTribe(23 + '');
-      expect(metricsResponse).toEqual([mockRepositoriesByTribe]);
+      await service.findRepositoriesByTribe(23 + '');
     } catch (e) {
       expect(e.message).toEqual(
         'La Tribu no tiene repositorios que cumplan con la cobertura necesaria',
